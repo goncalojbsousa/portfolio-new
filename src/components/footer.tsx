7143c089f05f9e4bb75b18dd9a19fc069a210bfe
zouker <goncalojbsousa@gmail.com>
2024-09-15 20:19:11 +0100

    add footer base

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <div className="bg-neutral-950 text-white border-t border-sky-950">
+            <footer className="flex justify-between mx-auto max-w-4xl lg:text-lg p-4">
+                <div>
+                    Gonçalo Sousa 2024
+                </div>
+                <div>
+                    Social media
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
