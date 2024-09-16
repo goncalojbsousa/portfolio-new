da73c3c322dcf937e138e3d1138d593500057c0c
zouker <goncalojbsousa@gmail.com>
2024-09-16 19:14:12 +0100

    add language switcher on footer and link to homepage on navbar image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,41 @@
-import { Link } from "@/i18n/routing";
+'use client'
+
+import { Link, usePathname } from "@/i18n/routing";
 import { WebsiteIcon } from "./svg/social-icon/website-icon";
 import { LinkedinIcon } from "./svg/social-icon/linkedin-icon";
 import { GithubIcon } from "./svg/social-icon/github-icon";
+import Image from "next/image";
 
 function Footer() {
+    const pathname = usePathname();
     return (
-        <div className="bg-neutral-950 text-white border-t border-sky-950">
+        <div className="bg-neutral-950 text-white border-t border-sky-950 mt-8">
             <footer className="flex justify-between mx-auto max-w-4xl lg:text-lg p-4">
-                <div>
-                    Gonçalo Sousa 2024
+                <div className='space-x-4 flex items-center'>
+                    <Link href={pathname} locale='en'>
+                        <Image
+                            className="rounded-full"
+                            src="/us.svg"
+                            alt="USA flag"
+                            width={30}
+                            height={30}
+                            draggable={false}
+                        />
+                    </Link>
+                    <Link href={pathname} locale='pt'>
+                        <Image
+                            className="rounded-full"
+                            src="/pt.svg"
+                            alt="PT flag"
+                            width={30}
+                            height={30}
+                            draggable={false}
+                        />
+                    </Link>
                 </div>
+
+                <p className="flex items-center">© 2024 Gonçalo Sousa</p>
+
                 <div className="flex space-x-4 items-center">
                     <Link href="https://goncalosousa.netlify.app" target="_blank">
                         <WebsiteIcon className="fill-white transition-transform hover:scale-110" />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,15 +11,17 @@ function Navbar() {
     return (
         <div className="fixed top-0 left-0 right-0 z-10 bg-neutral-950 bg-opacity-90 text-white border-b border-sky-950">
             <nav className="flex justify-between mx-auto max-w-4xl lg:text-lg">
-                <div className="flex items-center p-4">
-                    <Image
-                        className="rounded-full mr-4 border-2 border-blue-400"
-                        src="/photo.jpeg"
-                        alt="Gonçalo Sousa Photo"
-                        width={60}
-                        height={60}
-                        draggable={false}
-                    />
+                <div className="flex items-center p-3">
+                    <Link href='/'>
+                        <Image
+                            className="rounded-full mr-4 border-2 border-blue-400"
+                            src="/photo.jpeg"
+                            alt="Gonçalo Sousa Photo"
+                            width={60}
+                            height={60}
+                            draggable={false}
+                        />
+                    </Link>
                     <div>
                         <h1><b>Gonçalo Sousa</b></h1>
                         <p>{t('role')}</p>
