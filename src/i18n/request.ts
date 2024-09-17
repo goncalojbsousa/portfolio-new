807581900f0e18348f470647545e9bef031bcc7f
zouker <goncalojbsousa@gmail.com>
2024-09-17 02:54:53 +0100

    update type i18n request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,14 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
- 
-export default getRequestConfig(async ({locale}) => {
+import { notFound } from 'next/navigation';
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
+
+// Define the type for the locale
+type Locale = typeof routing.locales[number];
+
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as any)) notFound();
- 
+  if (!routing.locales.includes(locale as Locale)) notFound();
+
   return {
     messages: (await import(`../../messages/${locale}.json`)).default
   };
